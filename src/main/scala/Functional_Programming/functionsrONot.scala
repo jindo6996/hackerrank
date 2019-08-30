577b58d0ddb8ee3640b957f47244f72737f817f2
Nguyen Tai Dai <daint13@viettel.com>
2019-08-30 15:29:14 +0700

    tmp

---
+++ src/main/scala/Functional_Programming/functionsrONot.scala
@@ -0,0 +1,21 @@
+package Functional_Programming
+
+import scala.collection.mutable.ArrayBuffer
+
+object functionsrONot {
+  def main(args: Array[String]): Unit = {
+    val stdin = scala.io.StdIn
+
+    val T = stdin.readLine.trim.toInt
+
+    for (nItr <- 1 to T) {
+      val dd = ArrayBuffer.fill(500){}
+      val n = stdin.readLine.trim.toInt
+      for (iN <- 1 to n) {
+        val xy = stdin.readLine.trim.split(" ").map(_.toInt)
+//        if (dd(xy(0)).isNaN) dd. =  xy(1)
+
+      }
+    }
+  }
+}
