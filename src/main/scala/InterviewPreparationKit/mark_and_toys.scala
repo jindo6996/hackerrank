d0650440b842430b0e5b2d608ecf7a243ad9e852
Nguyen Tai Dai <daint13@viettel.com>
2019-10-21 16:41:24 +0700

    add mark and toys solution

---
+++ src/main/scala/InterviewPreparationKit/mark_and_toys.scala
@@ -0,0 +1,44 @@
+package InterviewPreparationKit
+
+import java.io._
+import java.math._
+import java.security._
+import java.text._
+import java.util._
+import java.util.concurrent._
+import java.util.function._
+import java.util.regex._
+import java.util.stream._
+
+object mark_and_toys {
+  // Complete the maximumToys function below.
+  def maximumToys(prices: Array[Int], k: Int): Int = {
+    val pricesSorted = prices.sorted
+    var sum = 0
+    var i = 0
+    while (sum + pricesSorted(i) < k) {
+      sum += pricesSorted(i)
+      i += 1
+    }
+    i
+  }
+
+  def main(args: Array[String]) {
+    val stdin = scala.io.StdIn
+
+    val printWriter = new PrintWriter("output.txt")
+
+    val nk = stdin.readLine.split(" ")
+
+    val n = nk(0).trim.toInt
+
+    val k = nk(1).trim.toInt
+
+    val prices = stdin.readLine.split(" ").map(_.trim.toInt)
+    val result = maximumToys(prices, k)
+
+    printWriter.println(result)
+
+    printWriter.close()
+  }
+}
